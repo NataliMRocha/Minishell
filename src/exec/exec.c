a3b567a1c4acdc8397062f439f1831378685b203
Everton <egeraldo@student.42sp.org.br>
2024-02-26 11:33:53 -0300

    tentando iniciar o exec

---
+++ src/exec/exec.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egeraldo <egeraldo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 16:08:00 by egeraldo          #+#    #+#             */
+/*   Updated: 2024/02/23 18:16:02 by egeraldo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void exec(t_ast *root)
+{
+	t_envs *paths;
+	char **path;
+	char *tmp;
+	int i;
+
+	paths = ft_getenv("PATH");
+	path = ft_split(paths->value, ':');
+	i = -1;
+	while (path && path[++i])
+		path[i] = ft_strjoin(path[i], "/", 0);
+	i = 0;
+	tmp = ft_strjoin(path[i], root->command_list[0], 0);
+	while(path[i] && access(tmp, F_OK) != 0)
+	{
+		tmp = ft_strjoin(path[i], root->command_list[0], 0);
+		i++;
+	}
+	i = fork();
+	if (i == 1)
+		if (execve(tmp, root->command_list, NULL) < 0)
+			printf("deu ruim");
+}
+
+void handle_and_or(t_ast *root)
+{
+	t_envs *status_code;
+
+	status_code = ft_getenv("?");
+	exec(root->left);
+	if((!status_code->value && root->type == AND))
+		exec(root->right);
+	else if (status_code->value && root->type == OR)
+		exec(root->right);
+}
+
+void starting_exec(t_ast *root)
+{
+	if(root->type == AND || root->type == OR)
+		handle_and_or(root);
+}
