fdcd3fbe800d51d856e07a7f4c81f8e09a7bae9c
Natali <namoreir@student.42.fr>
2024-03-04 12:51:04 -0300

    arrumando redirect

--- src/redirect/redirect.c
+++ src/redirect/redirect.c
@@ -6,7 +6,7 @@
 /*   By: natali <natali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 18:01:00 by egeraldo          #+#    #+#             */
-/*   Updated: 2024/03/04 12:14:03 by natali           ###   ########.fr       */
+/*   Updated: 2024/03/04 12:47:39 by natali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,16 @@ int    is_redir_in(t_ast *root)
 {
     if (!root->fd)
         root->fd = open(root->command_list[0], O_RDONLY);
-    if (!root->fd)
-        root->fd = open(root->command_list[0], O_RDONLY);
     if (root->fd < 0)
     {
-        update_status_error(0);
-        errno = 0;
-        errno = 0;
-        access(root->command_list[0], F_OK);
-        if (errno == ENOENT)
+        update_status_error(1);
+        if (access(root->command_list[0], F_OK))
         {
             ft_putstr_fd(root->command_list[0], 2);
             ft_putstr_fd(": No such file or directory\n", 2);
+            return (0);
         }
-        access(root->command_list[0], W_OK | R_OK);
-        if (errno == EACCES)
+        if (access(root->command_list[0], W_OK | R_OK))
         {
             ft_putstr_fd(root->command_list[0], 2);
             ft_putstr_fd(": Permission denied\n", 2);
@@ -52,26 +47,29 @@ int    is_redir_in(t_ast *root)
 }
 
 int    is_redir_out(t_ast *root)
-int    is_redir_out(t_ast *root)
 {
     if (!root->fd)
+    {    
+        //root = root->right;
         root->fd = open(root->command_list[0], O_RDWR | O_CREAT | O_TRUNC, 0666);
-    if (!root->fd)
-        root->fd = open(root->command_list[0], O_RDWR | O_CREAT | O_TRUNC, 0666);
+    }
+/*     else if (!root->right->fd && root->type == REDIR_APPEND)
+    {
+        root = root->right;
+        root->fd = open(root->command_list[0], O_RDWR | O_CREAT | O_APPEND, 0666);
+    } */
     if (root->fd < 0)
     {
-        update_status_error(0);
-        errno = 0;
         errno = 0;
         access(root->command_list[0], W_OK | R_OK);
         if (errno == EACCES)
         {
             ft_putstr_fd(root->command_list[0], 2);
             ft_putstr_fd(": Permission denied\n", 2);
+            update_status_error(1);
         }
         return (0);
     }
-    dup2(root->fd, STDOUT_FILENO);
     dup2(root->fd, STDOUT_FILENO);
     close(root->fd);
     return (1);
@@ -87,7 +85,7 @@ void    handle_redir(t_ast *root)
         handle_redir(root->left);
     if (root->type == REDIR_OUT || root->type == REDIR_APPEND)
     {
-        if (!is_redir_out(root->right))
+        if (!is_redir_out(root))
         {
 		    close_fds(std_fd, 0);
             return ;
@@ -98,13 +96,11 @@ void    handle_redir(t_ast *root)
         if (!is_redir_in(root->right))
         {
 		    close_fds(std_fd, 0);
-		    close_fds(std_fd, 0);
             return ;
         }
     }
     starting_exec(root->left);
     dup2(std_fd[1], STDOUT_FILENO); 
     dup2(std_fd[0], STDIN_FILENO);
-    close_fds(std_fd, 0);
     close_fds(std_fd, 0);
 }
