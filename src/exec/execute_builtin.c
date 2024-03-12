7a23505f1abf54654b0db489c79c54ea89c23345
Everton <egeraldo@student.42sp.org.br>
2024-03-11 23:05:00 -0300

    resolvendo erros ao executar echo

--- src/exec/execute_builtin.c
+++ src/exec/execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: etovaz <etovaz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 17:15:02 by egeraldo          #+#    #+#             */
-/*   Updated: 2024/03/11 22:31:25 by etovaz           ###   ########.fr       */
+/*   Updated: 2024/03/11 23:04:04 by etovaz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,16 @@ int	execute_builtin(t_ast *root)
 		return (ft_unset(root->cmd_list));
 	if (!ft_strncmp(root->cmd_list[0], "env", 3))
 		return (print_env_list(*create_envs_table(1, 0)));
+	if (!ft_strncmp(root->cmd_list[0], "pwd", 2))
+		return (ft_pwd());
+	if (!ft_strncmp(root->cmd_list[0], "cd", 2))
+		return (ft_cd(root->cmd_list[1]));
+	if (!ft_strncmp("echo", root->cmd_list[0], ft_strlen(root->cmd_list[0])))
+		return (ft_echo(root->cmd_list));
 	if (!ft_strncmp(root->cmd_list[0], "exit", 4))
 	{
 		ft_exit(root->cmd_list, root);
 		return(0);
 	}
-	if (!ft_strncmp(root->cmd_list[0], "pwd", 2))
-		return (ft_pwd());
-	if (!ft_strncmp(root->cmd_list[0], "cd", 2))
-		return (ft_cd(root->cmd_list[1]));
-	if (!ft_strncmp(root->cmd_list[0], "echo", 4))
-		return (ft_echo(root->cmd_list));
 	return (1);
 }
