78b2f387c787eb854475fedb9a56290731193554
Everton <egeraldo@student.42sp.org.br>
2024-03-19 10:35:51 -0300

    arrumando o exit

--- src/builtin/ft_exit.c
+++ src/builtin/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etovaz <etovaz@student.42.fr>              +#+  +:+       +#+        */
+/*   By: egeraldo <egeraldo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 18:27:31 by etovaz            #+#    #+#             */
-/*   Updated: 2024/03/18 22:01:48 by etovaz           ###   ########.fr       */
+/*   Updated: 2024/03/19 10:33:11 by egeraldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,17 +49,19 @@ int	handle_exit_error(char **prompt)
 		tmp = &prompt[1][1];
 	else
 		tmp = prompt[1];
-	if ((prompt && tmp) && ft_strlen(tmp) != ft_intlen(nb))
+	if ((prompt && tmp && ft_strlen(tmp) != ft_intlen(nb)) || (tmp && !ft_isnum(tmp)))
 	{
-		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+		ft_putstr_fd("exit\nminishell: exit: ", STDERR_FILENO);
 		ft_putstr_fd(tmp, STDERR_FILENO);
 		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-		return (2);
+		update_status_error(2);
+		return (-2);
 	}
 	if (ft_splitlen(prompt) > 2)
 	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
-		return (1);
+		ft_putstr_fd("exit\nminishell: exit: too many arguments\n", STDERR_FILENO);
+		update_status_error(1);
+		return (-1);
 	}
 	return (nb);
 }
@@ -69,15 +71,19 @@ void	ft_exit(char **prompt, t_ast *root)
 	int	i;
 	int	exit_status;
 
-	free_env_list(*create_envs_table(1, 1));
 	exit_status = handle_exit_error(prompt);
-	save_fds(NULL, 1);
-	ast_holder(root, 1, 1);
-	if (!exit_status && !on_heredoc(-1))
-		ft_putstr_fd("exit\n", STDERR_FILENO);
-	i = 0;
-	while (i < 3)
-		close(i++);
-	rl_clear_history();
-	exit(update_status_error(exit_status));
+	if (exit_status != -1)
+	{
+		free_env_list(*create_envs_table(1, 1));
+		save_fds(NULL, 1);
+		ast_holder(root, 1, 1);
+		if (exit_status > 0 && !on_heredoc(-1))
+			ft_putstr_fd("exit\n", STDERR_FILENO);
+		i = 0;
+		while (i < 3)
+			close(i++);
+		rl_clear_history();
+		exit(update_status_error(exit_status));
+	}
+	return ;
 }
